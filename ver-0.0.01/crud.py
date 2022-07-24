5904c45785f4b2a239098535878b0bdbd2daf4b3
Raphael Gomes <raphaelfsg@gmail.com>
2022-07-23 23:55:57 -0300

    primeiro codigo

---
+++ ver-0.0.01/crud.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+with st.from(key="include_cliente"):
+    input_name = st.text_input(label="Insira o seu nome")
+    input_age = st.number_input(label="Insira a sua idade")
+    input_occupation = st.selectbox("Selecione a sua profissão", ["Desenvolvedor", "Designer", "Orientador"])
+    input_button_submit = st.form_submit_button("Enviar")
