da6b639102f85145c24684c9e2c466cc1a9eb232
Raphael Gomes <raphaelfsg@gmail.com>
2022-07-24 00:01:24 -0300

    correção

--- ver-0.0.01/crud.py
+++ ver-0.0.01/crud.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 with st.form(key="include_cliente"):
     input_name = st.text_input(label="Insira o seu nome")
-    input_age = st.number_input(label="Insira a sua idade")
+    input_age = st.number_input(label="Insira a sua idade", format="%d", step=1)
     input_occupation = st.selectbox("Selecione a sua profissão", ["Desenvolvedor", "Designer", "Orientador"])
     input_button_submit = st.form_submit_button("Enviar")
