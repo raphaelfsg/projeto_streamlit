8f22d46199af3fa685bbde5a4a16963b893416e8
Raphael Gomes <raphaelfsg@gmail.com>
2022-07-25 10:51:53 -0300

    mods

--- ver-0.0.01/main.py
+++ ver-0.0.01/main.py
@@ -1,14 +1,12 @@
 import streamlit as st
 
-st.title("Incluir cliente")
+st.title("Project team")
 
-with st.form(key="include_cliente"):
+with st.form(key="include_cliente", clear_on_submit=True):
     input_name = st.text_input(label="Insira o seu nome")
     input_age = st.number_input(label="Insira a sua idade", format="%d", step=1)
     input_occupation = st.selectbox("Selecione a sua profissão", ["Desenvolvedor", "Designer", "Orientador"])
     input_button_submit = st.form_submit_button("Enviar")
 
 if input_button_submit:
-    st.write(f'Nome: {input_name}')
-    st.write(f'Idade: {input_age}')
-    st.write(f'Profissão: {input_occupation}')
+    
---
+++ ver-0.0.01/models/p_team.py
@@ -0,0 +1,5 @@
+class p_team:
+    def __init__(self, p_name, p_age, p_occupation):
+        self.p_name = p_name
+        self.p_age = p_age
+        self.p_occupation = p_occupation
