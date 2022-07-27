cfc5c76ac300bea05285ef691551bf8238e96a29
Raphael Gomes <raphaelfsg@gmail.com>
2022-07-27 16:31:51 -0300

    att

--- ver-0.0.01/Controllers/UserController.py
+++ ver-0.0.01/Controllers/UserController.py
@@ -1,2 +1,10 @@
-import services.database as db;
+import services.database as db
+
+def incluir() {
+    count = cursor.execute("""
+    INSERT INTO project_user (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) 
+    VALUES (?,?,?,?,?)""",
+    'SQL Server Express New 20', 'SQLEXPRESS New 20', 0, 0, CURRENT_TIMESTAMP).rowcount
+    cnxn.commit()
+}
 
